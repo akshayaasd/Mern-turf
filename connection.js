d66b28e5df62161b54dfaac55633b19af6a2055f
Akshayaa <152466738+akshayaasd@users.noreply.github.com>
2024-06-14 09:47:32 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connect = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/bookturf', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+module.exports = connect;
